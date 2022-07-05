37b6fe4dcff9c2e11e98b76d1fa1bb63d14d3a46
Jorgvt <vila_tronik@hotmail.com>
2022-07-05 17:53:28 +0200

    :sparkles: Added CrispeningRedGreenTest class.

--- perceptualtests/tests.py
+++ perceptualtests/tests.py
@@ -65,8 +65,8 @@ class CrispeningAchromaticTest(PsychoTest):
                 self._stimuli[idx_bg] = colored_squares
         return self._stimuli
 
-    def show_stimuli(self):
-        fig, axis = plt.subplots(*self.stimuli.shape[:2])
+    def show_stimuli(self, **kwargs):
+        fig, axis = plt.subplots(*self.stimuli.shape[:2], **kwargs)
         for i,ax_bg in enumerate(axis):
             for j, ax_t in enumerate(ax_bg):
                 ax_t.imshow(self.stimuli[i,j])
@@ -101,6 +101,82 @@ class CrispeningAchromaticTest(PsychoTest):
             plt.plot(np.cumsum(dist), readout, 'o-', color=color, alpha=alpha, label=f'{atd_bg[i]} $cd/m^2$')
         plt.xlabel(r'$|\mathrm{\vec{n}}|$ (Digital Values)')
         plt.legend(title = 'Background luminance')
+
+    def test(self, model):
+        readouts = self.get_readouts(model)
+        self.plot_result(readouts)
+
+class CrispeningRedGreenTest(PsychoTest):
+    """
+    Test to check the nonlinearities in the Red-Green channel.
+    The test is performed by creating a set of images with two colors:
+    background and central.
+    Its result is obtained by measuring the visibility of each image
+    with respect to an image with only the background color.
+    """
+    def __init__(self, N, img_size, square_size):
+        self.N = N
+        self.img_size = img_size
+        self.square_size = square_size
+        self._stimuli = None
+
+    @property
+    def stimuli(self):
+        if self._stimuli is None:
+            atd = np.array([[60,a,0] for a in np.linspace(-20,20,self.N)]).T
+            bg_idx = np.array([6, 8, 11, 14, 16])-1
+            atd_bg = np.array([[60*1.8,a,0] for a in np.linspace(-20,20,self.N)[bg_idx]]).T
+            rgb = atd2rgb(atd)
+            rgb_bg = atd2rgb(atd_bg)
+            self._stimuli = np.empty(shape=(len(atd_bg.T),self.N,*self.img_size,3))
+            for idx_bg, bgc in enumerate(rgb_bg.T):
+                colored_squares = np.empty(shape=(self.N,*self.img_size,3))
+                ## Generate all the colored squares with a fixed bg_color
+                for i, sqc in enumerate(rgb.T):
+                    colored_square = create_colored_square(img_size=self.img_size,
+                                                           square_size=self.square_size,
+                                                           square_color=sqc,
+                                                           bg_color=bgc)
+                    colored_squares[i] = colored_square
+                self._stimuli[idx_bg] = colored_squares
+        return self._stimuli
+
+    def show_stimuli(self, **kwargs):
+        fig, axis = plt.subplots(*self.stimuli.shape[:2], **kwargs)
+        for i,ax_bg in enumerate(axis):
+            for j, ax_t in enumerate(ax_bg):
+                ax_t.imshow(self.stimuli[i,j])
+                ax_t.axis('off')
+
+    def get_readouts(self, model):
+        bg_idx = np.array([6, 8, 11, 14, 16])-1
+        all_readouts = np.empty(shape=self.stimuli.shape[:2])
+        for idx_bg, (i, imgs_samebg)in zip(bg_idx, enumerate(self.stimuli)):
+            outputs = model.predict(imgs_samebg)
+            readouts = (outputs-outputs[idx_bg])**2
+            readouts = np.sqrt(np.sum(readouts, axis=(1,2,3)))
+            signs = np.ones_like(readouts)
+            signs[:idx_bg] = -1
+            readouts = readouts*signs
+            all_readouts[i] = readouts
+        return all_readouts
+
+    def plot_result(self, readouts):
+        ## Obtain the transform-corrected xaxis
+        atd = np.array([[60,a,0] for a in np.linspace(-20,20,self.N)]).T
+        bg_idx = np.array([6, 8, 11, 14, 16])-1
+        atd_bg = np.array([[60*1.8,a,0] for a in np.linspace(-20,20,self.N)[bg_idx]]).T
+        a_ng = (Mxyz2ng @ Matd2xyz@ atd)**(1/2)
+        dist = ((a_ng.T[:-1]-a_ng.T[1:])**2).sum(axis=1)**(1/2)
+        dist = np.concatenate([np.array([0]), dist])
+        color = ['green', 'green', 'black', 'red', 'red']
+        alpha = [1.0, 0.5, 1.0, 0.5, 1.0]
+        for i, readout in enumerate(readouts):
+            plt.plot(np.cumsum(dist), readout, 'o-', 
+                     color=color[i], alpha=alpha[i], 
+                     label=' ')
+        plt.xlabel(r'$|\mathrm{\vec{n}}|$ (Digital Values)')
+        plt.legend(title = 'Background color')
 
     def test(self, model):
         readouts = self.get_readouts(model)
